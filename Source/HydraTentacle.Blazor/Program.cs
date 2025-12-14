2aec6dc85e51467ddf144a35ab0014e88590af37
koray arar <arargame@hotmail.com>
2025-12-14 04:44:02 +0300

    test commit

--- Source/HydraTentacle.Blazor/Program.cs
+++ Source/HydraTentacle.Blazor/Program.cs
@@ -8,6 +8,7 @@ namespace HydraTentacle.Blazor
     {
         public static void Main(string[] args)
         {
+            //test
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
--- Source/HydraTentacle.WebApi/Program.cs
+++ Source/HydraTentacle.WebApi/Program.cs
@@ -5,6 +5,7 @@ namespace HydraTentacle.WebApi
     {
         public static void Main(string[] args)
         {
+            //test
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
